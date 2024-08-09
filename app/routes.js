43fa1dd92aa4b7d3908244b629fb8e2bf57a808f
efrenjr-dev <efrenjr.rodriguez@gmail.com>
2024-08-09 20:11:11 +0800

    routes with validator

--- app/routes.js
+++ app/routes.js
@@ -4,23 +4,58 @@ export default function (app) {
     app.get("/currency", (req, res) => {
         res.status(200).send({ currencies: exchangeRates });
     });
+    app.post("/currency", (req, res) => {
+        if (!req.body.hasOwnProperty("name")) {
+            return res.status(400).send({
+                error: "Bad Request: missing required parameter NAME",
+            });
+        }
+        if (typeof req.body.name !== "string") {
+            return res.status(400).send({
+                error: "Bad Request: NAME has to be a string",
+            });
+        }
+        if (req.body.name.length < 1) {
+            return res.status(400).send({
+                error: "Bad Request: NAME should not be an empty string",
+            });
+        }
+        if (!req.body.hasOwnProperty("ex")) {
+            return res
+                .status(400)
+                .send({ error: "Bad Request: missing required parameter EX" });
+        }
+        if (typeof req.body.ex !== "object") {
+            return res
+                .status(400)
+                .send({ error: "Bad Request: EX has to be an object" });
+        }
+        if (Object.keys(req.body.ex).length === 0) {
+            return res.status(400).send({
+                error: "Bad Request: EX should not be an empty object",
+            });
+        }
+        if (!req.body.hasOwnProperty("alias")) {
+            return res.status(400).send({
+                error: "Bad Request: missing required parameter ALIAS",
+            });
+        }
+        if (typeof req.body.alias !== "string    ") {
+            return res
+                .status(400)
+                .send({ error: "Bad Request: ALIAS has to be a string" });
+        }
+        if (req.body.alias.length < 1) {
+            return res.status(400).send({
+                error: "Bad Request: ALIAS should not be an empty string",
+            });
+        }
+    });
 }
 
 /*
 
-error: "Bad Request : missing required parameter NAME",
 
-error: "Bad Request : NAME has to be a string",
-
-error: "Bad Request : NAME should not be an empty string",
-
-error: "Bad Request : missing required parameter EX",
-
-error: "Bad Request : EX has to be an object",
-
-error: "Bad Request : EX should not be an empty object",
-
-error: "Bad Request : missing required parameter ALIAS",
 
 error: "Bad Request : ALIAS has to be a string",
 
